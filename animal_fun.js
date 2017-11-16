4fddbe56cdb3df477287386d5f5fe0a2dc45a2fe
DeliSauce <nycdelisauce@gmail.com>
2017-11-16 13:15:22 -0800

    Allow user to input a single letter and create a new file with animals beginning with that letter.

--- animal_fun.js
+++ animal_fun.js
@@ -1,51 +1,53 @@
 const fs = require('fs');
 const http = require('http');
 
-const letter = process.argv[2];
-// console.log(letter);
+//Read a file and output to the console.
+// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
 
-let grab = [];
-//
-//
-//
+//Print the global process object to console: contains a lot of info about node process
+// console.log(process);
+
+const letter = process.argv[2];
+let newList = [];
+
+if (letter) {
+  fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      data = data.split('\n');
+      data.forEach( word => {
+        if (word.slice(0,1) === letter.toUpperCase()) {
+          newList.push(word);
+        }
+      });
+
+      fs.writeFile(`${letter}_animals.txt`, newList.join("\n"), (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("file successfully written!");
+        }
+      });
+    }
+  });
+} else {
+  console.log("You need to enter a letter after 'node animal_fun.js '");
+}
+
+
+
+
+
 // const server = http.createServer((req, res) => {
 //     res.write('hello world');
 //     res.end();
 // });
 //
 // server.listen(8000, () => console.log("I'm listening on port 8000!"));
-
-
-//Read a file and output to the console.
-fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
-
-
-// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     data = data.split('\n');
-//     data.forEach(word => {
-//       if (word.slice(0,1) === letter.toUpperCase()) {
-//         grab.push(word);
-//         // console.log(word);
-//       }
-//     });
-//
-//     fs.writeFile(`${letter}_animals.txt`, grab.join("\n"), err => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log("file successfully written!");
-//       }
-//     });
-//
-//
-//   }
-// });
